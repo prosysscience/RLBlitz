2cd92cc1dbb44e12258caa94f66107b1358aabb2
Pierre TASSEL <pierre.tassel@outlook.fr>
2021-02-04 18:12:04 +0100

    #2 need more tests

--- utils/Diverse.py
+++ utils/Diverse.py
@@ -35,3 +35,15 @@ def optimizer_to(optim, device):
                     subparam.data = subparam.data.to(device, non_blocking=True)
                     if subparam._grad is not None:
                         subparam._grad.data = subparam._grad.data.to(device, non_blocking=True)
+    for param in optim.param_groups.values():
+        # Not sure there are any global tensors in the state dict
+        if isinstance(param, torch.Tensor):
+            param.data = param.data.to(device, non_blocking=True)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device, non_blocking=True)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, torch.Tensor):
+                    subparam.data = subparam.data.to(device, non_blocking=True)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device, non_blocking=True)
