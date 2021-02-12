#12 fix kl div + no grad

agents/ppo.py
@@ -22,11 +22,11 @@ class PPO(A2C):
             wandb.log(self.statistics.start_inference(), step=self.statistics.get_iteration())
             with torch.no_grad():
                 probabilities, values = self.inference_model(self.states_tensor)
-            wandb.log(self.statistics.end_inference(), step=self.statistics.get_iteration())
-            dist = self.distribution(probabilities)
-            actions = dist.sample()
-            log_prob = dist.log_prob(actions)
-            actions = actions.to('cpu', non_blocking=True)
+                wandb.log(self.statistics.end_inference(), step=self.statistics.get_iteration())
+                dist = self.distribution(probabilities)
+                actions = dist.sample()
+                log_prob = dist.log_prob(actions)
+                actions = actions.to('cpu', non_blocking=True)
 
             self.memory.states[step_nb] = self.states_tensor.to(self.training_device, non_blocking=True)
             self.memory.actions[step_nb] = actions.to(self.training_device, non_blocking=True)
@@ -60,7 +60,10 @@ class PPO(A2C):
         with torch.no_grad():
             advantage = self._compute_advantage(self.config['use_gae'])
         accumulated_kl_div = 0
+        number_epoch_done = 0
         for epoch in range(self.ppo_epoch):
+            accumulated_epoch_kl_div = 0
+            nb_mini_batches_done = 0
             with torch.no_grad():
                 permutation = torch.randperm(self.memory.states.size(0) - 1)
             for i in range(0, self.memory.states.size(0) - 1, self.mini_batch_size):
@@ -70,7 +73,8 @@ class PPO(A2C):
 
                 mini_batch_advantage = advantage[indices, :]
                 if self.config['normalize_advantage']:
-                    mini_batch_advantage = (mini_batch_advantage - mini_batch_advantage.mean()) / (mini_batch_advantage.std() + 1e-8)
+                    with torch.no_grad():
+                        mini_batch_advantage = (mini_batch_advantage - mini_batch_advantage.mean()) / (mini_batch_advantage.std() + 1e-8)
 
                 new_probabilities, new_values = self.training_model(self.memory.states[indices, :])
                 new_values = new_values.view(new_values.shape[0], new_values.shape[1])
@@ -91,11 +95,14 @@ class PPO(A2C):
                     torch.nn.utils.clip_grad_norm_(self.training_model.parameters(), self.config['clip_grad_norm'])
                 self.optimizer.step()
                 with torch.no_grad():
-                    epoch_kl = torch.mean(self.memory.logprobs[indices, :] - new_probabilities).cpu()
-                    accumulated_kl_div += epoch_kl
-                if self.target_kl_div is not None and (accumulated_kl_div / (epoch + 1) > 1.5) * self.target_kl_div:
-                    break
-        wandb.log({'kl_div_iter': accumulated_kl_div / self.ppo_epoch}, step=self.statistics.get_iteration())
+                    mini_batch_kl = torch.mean(self.memory.logprobs[indices, :] - new_probabilities).cpu()
+                    accumulated_epoch_kl_div += mini_batch_kl
+                nb_mini_batches_done += 1
+            number_epoch_done += 1
+            accumulated_kl_div += (accumulated_epoch_kl_div / nb_mini_batches_done)
+            if self.target_kl_div is not None and (accumulated_kl_div / (number_epoch_done + 1)) > 1.5 * self.target_kl_div:
+                break
+        wandb.log({'kl_div_iter': accumulated_kl_div / number_epoch_done, 'epoch_this_iter': number_epoch_done}, step=self.statistics.get_iteration())
         self.scheduler.step()
         self.states_tensor = self.states_tensor.to(self.inference_device, non_blocking=True)
         self.inference_model.load_state_dict(self.training_model.state_dict())

configs/a2c_default.py
@@ -86,8 +86,12 @@ default_a2c_config = {
             nn.Softmax(dim=1)
         ),
     },
-    'critic_layers_initialization': lambda x: init_weights(x, function_output=lambda x: default_actor_critic(x, std=1.0)),
-    'actor_layers_initialization': init_weights,
+
+    'critic_layers_initialization': lambda x: init_weights(x,
+                                                           function_output=lambda x: default_actor_critic(x, gain=1.00)),
+    'actor_layers_initialization': lambda x: init_weights(x,
+                                                           function_output=lambda x: default_actor_critic(x, gain=0.01)),
+
 
     # ADVANCED CONFIG (don't touch if you don't know what you're doing)
     # VecEnv option, don't touch if not needed

configs/ppo_default.py
@@ -23,7 +23,7 @@ default_ppo_config = {
 
     # NN CONFIG
     'optimizer': optim.Adam,  # if you need more control, you can define a lambda
-    'lr_initial': 1e-4,
+    'lr_initial': 5e-5,
     # None means constant
     # if you want to use Pytorch scheduler, you can! It's even recommended
     # Example: 'lr_scheduler': lambda x: torch.optim.lr_scheduler.LambdaLR(x, lr_lambda=lambda epoch: 0.999**epoch),
@@ -49,17 +49,17 @@ default_ppo_config = {
     'WandB_notes': None,
     # define the frequency WandB logs gradients from model
     # don't set too low or you will reach the api limits
-    'WandB_model_log_frequency': 100,
+    'WandB_model_log_frequency': 300,
 
     # Actor Critic specific config
     'distribution': Categorical,
     # Lamdba-GAE: https://arxiv.org/abs/1506.02438
     'use_gae': True,
-    'lambda_gae': 0.95,
+    'lambda_gae': 0.98,
     # only valuable with large batches
     'normalize_advantage': True,
     'vf_coeff': 1.0,
-    'entropy_coeff': 1e-3,
+    'entropy_coeff': 1e-4,
     # neural network
     # define the template needs to inherit from AbstractActorCritic
     # can be change if you need very specific behavior
@@ -88,16 +88,17 @@ default_ppo_config = {
         ),
     },
     'critic_layers_initialization': lambda x: init_weights(x,
-                                                           function_output=lambda x: default_actor_critic(x, std=1.0)),
-    'actor_layers_initialization': init_weights,
+                                                           function_output=lambda x: default_actor_critic(x, gain=1.00)),
+    'actor_layers_initialization': lambda x: init_weights(x,
+                                                           function_output=lambda x: default_actor_critic(x, gain=0.01)),
 
     # PPO Specific config
-    'ppo_epochs': 10,
-    'clipping_param': 0.2,
-    'mini_batch_size': 32,
+    'ppo_epochs': 30,
+    'clipping_param': 0.3,
+    'mini_batch_size': 128,
     'min_reward': -10,
     'max_reward': 10,
-    'target_kl_div': None,
+    'target_kl_div': 0.01,
 
     # ADVANCED CONFIG (don't touch if you don't know what you're doing)
     # VecEnv option, don't touch if not needed

utils/Diverse.py
@@ -42,12 +42,12 @@ class MockLRScheduler:
         return [self.lr]
 
 
-def default_actor_critic(x, mean=0., std=0.1):
-    return nn.init.normal_(x, mean=mean, std=std)
+def default_actor_critic(x, gain=np.sqrt(2)):
+    return nn.init.orthogonal_(x, gain=gain)
 
 
-def init_weights(m, function_hidden=default_actor_critic, bias_hidden=0.1,
-                 function_output=lambda x: default_actor_critic, bias_output=0.1):
+def init_weights(m, function_hidden=default_actor_critic, bias_hidden=0.0,
+                 function_output=default_actor_critic, bias_output=0.1):
     linear_layers = [module for module in m.modules() if isinstance(module, nn.Linear)]
     for layers in linear_layers[:-1]:
         function_hidden(layers.weight)